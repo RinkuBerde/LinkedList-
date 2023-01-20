8c71399eaae4020c7702e0778df25ab3a9c83d0d
rinkuberde <rinkuberde@gmail.com>
2023-01-20 14:46:38 +0530

    [Rinku] - Welcome Message

---
+++ LinkedList-/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedList_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList Practice Problems!!!");
+        }
+    }
+}
